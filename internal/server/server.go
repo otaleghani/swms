f2585e125a246bcfdc9abc634a3bec58bad54ebe
Oliviero Taleghani <o.taleghani@talesign.com>
2024-04-30 09:54:52 +0200

    chore: comments on label

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,3 +1,13 @@
+/*********************************************************************
+
+We need to connect this with the other part of the application.
+
+1. Create media folder where all of the media is stored
+
+localhost:8000/qr/xxx.png
+-> This can be an example of url
+
+*********************************************************************/
 package server
 
 import(
@@ -17,6 +27,7 @@ func ServerStart(path string) {
   mux := http.NewServeMux()
 
   mux.HandleFunc("/add", handleAdd(db))
+  mux.HandleFunc("/media/*", http.FileServer(http.Dir("media")))
   mux.HandleFunc("/get", handleGet(db))
 
   corsMux := middlewareCors(mux)
