acd61f6bc75cfc6d664026214cda3c9b046da007
Oliviero Taleghani <o.taleghani@talesign.com>
2024-09-14 14:40:37 +0200

    wip: refactor patterns

--- internal/ui/app/ui/patterns/form/InputWrapper.tsx
+++ internal/ui/app/ui/patterns/form/InputWrapper.tsx
@@ -1,10 +1,94 @@
+/** Local components */
+import { Label } from "@/app/ui/components/label";
+import { Input } from "@/app/ui/components/input";
+import { Textarea } from "@/app/ui/components/textarea";
+import InputWrapperErrors from "./InputWrapperErrors";
+
 interface InputWrapperProps {
   field: 
     "name" |
-    "description";
-
+    "description" |
+    "identifier" |
+    "code" |
+    "width" |
+    "height" |
+    "weight" |
+    "length" |
+    "quantity";
+  errorMessages: string[];
+  dict: any;
+  defaultValue?: string;
+  className?: string;
+  label?: boolean;
 }
 
-export default function InputWrapper({}) {
+export default function InputWrapper({
+  field,
+  className,
+  errorMessages,
+  defaultValue,
+  label,
+  dict
+}: InputWrapperProps) {
+  const inputId = `id-${Date.now()}-${Math.random().toString(36).substring(2, 9)}`;
+  const InputWrapperField = () => {
+    switch (field) {
+      case "name":
+      case "identifier":
+      case "code":
+        return (
+          <Input
+            type="text"
+            defaultValue={defaultValue}
+            name={field}
+            id={`${field}-${inputId}`}
+            placeholder={dict.placeholder}
+          />
+        );
+      case "description":
+        return (
+          <Textarea
+            defaultValue={defaultValue}
+            name={field}
+            id={`${field}-${inputId}`}
+            placeholder={dict.placeholder}
+          />
+        );
+      case "width":
+      case "length":
+      case "weight":
+      case "length":
+        return (
+          <Input
+            type="number"
+            defaultValue={defaultValue}
+            name={field}
+            id={`${field}-${inputId}`}
+            placeholder={dict.placeholder}
+          />
+        );
+      case "quantity":
+        return (
+          <Input
+            type="number"
+            defaultValue={defaultValue}
+            name={field}
+            id={`${field}-${inputId}`}
+            placeholder={dict.placeholder}
+            step="1"
+          />
+        );
+    };
+  };
 
+  return (
+    <div className={className}>
+      {label && (<Label htmlFor={`${field}-${inputId}`}>{dict.label}</Label>)}
+      <InputWrapperField />
+      <InputWrapperErrors 
+        errorMessages={errorMessages}
+        id={`${field}-${inputId}-errors`}
+      />
+    </div>
+  );
 }
---
+++ internal/ui/app/ui/patterns/form/InputWrapperErrors.tsx
@@ -0,0 +1,31 @@
+import { AlertCircle } from "lucide-react";
+
+interface InputWrapperErrorsProps {
+  errorMessages: string[],
+  id: string,
+}
+
+export default function InputWrapperErrors({
+  errorMessages,
+  id
+}: InputWrapperErrorsProps) {
+  return (
+    <>
+      {errorMessages.length != 0 && (
+        <div 
+          id={id} 
+          aria-live="polite" 
+          aria-atomic="true" 
+          className="my-2 rounded-md p-3 gap-2 items-center flex border border-red-500 text-red-500"
+        >
+          <AlertCircle className="h-4 w-4" />
+          <div className="text-sm">
+            {errorMessages.map((error) => (
+              <span className="block text-left">{error}</span>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
